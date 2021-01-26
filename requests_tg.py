0f44a7ba3861022073f24cca154c84f362000351
Евгений <tizerofspar@yandex.ru>
2021-01-26 20:10:27 +0300

    Ресрукторизация проекта

---
+++ requests_tg.py
@@ -0,0 +1,28 @@
+import requests
+
+from settings import token
+
+URL = 'https://api.telegram.org/bot' + token + '/'
+
+
+def send_message(chat_id, text, reply_markup=''):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text, 'reply_markup': reply_markup}
+    r = requests.post(url, json=answer)
+    return r
+
+
+def deleteMessageReplyMarkup(chat_id, message_id):
+    url = URL + 'editMessageReplyMarkup'
+    answer = {'chat_id': chat_id,
+              'message_id': message_id, 'reply_markup': [[]]}
+    r = requests.post(url, json=answer)
+    return r
+
+
+def forward_message(chat_id, from_chat_id, message_id):
+    url = URL + 'forwardMessage'
+    answer = {'chat_id': chat_id,
+              'from_chat_id': from_chat_id, 'message_id': message_id}
+    r = requests.post(url, json=answer)
+    return r
